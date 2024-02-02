e63247a75b7af8af6be6794c1b26532e95161ce7
braceal <abrace05@gmail.com>
2024-02-01 21:19:30 -0600

    parsl update

--- protein_search/distributed_inference.py
+++ protein_search/distributed_inference.py
@@ -1,37 +1,43 @@
 from __future__ import annotations
-from accelerate import PartialState
-from transformers import EsmForMaskedLM, EsmTokenizer, PreTrainedModel, BatchEncoding
+from transformers import (
+    EsmForMaskedLM,
+    EsmTokenizer,
+    BatchEncoding,
+    PreTrainedModel,
+    PreTrainedTokenizer,
+)
 import torch
 import numpy as np
+import functools
 from tqdm import tqdm
 from pathlib import Path
-from dataclasses import dataclass, field
+from typing import Callable
+from argparse import ArgumentParser
 from torch.utils.data import Dataset, DataLoader
-from protein_search.utils import ArgumentsBase, read_fasta
-
+from parsl.concurrent import ParslPoolExecutor
+from protein_search.registry import register
+from protein_search.utils import BaseModel, read_fasta
+from protein_search.parsl import ComputeSettingsTypes
 
 # TODO: For big models, see here: https://huggingface.co/docs/accelerate/usage_guides/big_modeling
 # Documentation on using accelerate for inference: https://huggingface.co/docs/accelerate/usage_guides/distributed_inference
 # TODO: Skip the for loop over the sequence lenghts using the attention mask:
 #   https://stackoverflow.com/questions/65083581/how-to-compute-mean-max-of-huggingface-transformers-bert-token-embeddings-with-a
 
-# Initialize distributed state
-distributed_state = PartialState()
 
-
-class SequenceDataset(Dataset):
-    def __init__(self, sequences: list[str]) -> None:
-        self.sequences = sequences
+class InMemoryDataset(Dataset):
+    def __init__(self, data: list[str]) -> None:
+        self.data = data
 
     def __len__(self) -> int:
-        return len(self.sequences)
+        return len(self.data)
 
     def __getitem__(self, idx: int) -> str:
-        return " ".join(self.sequences[idx].upper())
+        return self.data[idx]
 
 
-class SequenceCollator:
-    def __init__(self, tokenizer: EsmTokenizer) -> None:
+class DataCollator:
+    def __init__(self, tokenizer: PreTrainedTokenizer) -> None:
         self.tokenizer = tokenizer
 
     def __call__(self, batch: list[str]) -> BatchEncoding:
@@ -43,9 +49,11 @@ def compute_avg_embeddings(
     model: PreTrainedModel, dataloader: DataLoader
 ) -> np.ndarray:
     """Function to compute averaged hidden embeddings."""
+    # TODO: Instead of using a list, store the embeddings in a torch tensor
+    # with the size reserved for the entire dataset.
     embeddings = []
     for batch in tqdm(dataloader):
-        batch = batch.to(distributed_state.device)
+        batch = batch.to(model.device)
         outputs = model(**batch, output_hidden_states=True)
         last_hidden_states = outputs.hidden_states[-1]
         seq_lengths = batch.attention_mask.sum(axis=1)
@@ -57,64 +65,110 @@ def compute_avg_embeddings(
     return np.array(embeddings)
 
 
-@dataclass
-class Arguments(ArgumentsBase):
-    input_dir: Path = field(
-        metadata={"help": "An input directory containing .fasta files"},
+def embed_file(
+    file: Path,
+    output_dir: Path,
+    model_id: str,
+    batch_size: int,
+    num_data_workers: int,
+    data_reader_fn: Callable[[Path], list[str]],
+    model_fn: Callable[[str], tuple[PreTrainedModel, PreTrainedTokenizer]],
+) -> None:
+    """Function to embed a single file and save a numpy array with embeddings."""
+    # Initialize the model and tokenizer
+    model, tokenizer = model_fn(model_id)
+
+    # Read the data
+    data = data_reader_fn(file)
+
+    # Build a torch dataset for efficient batching
+    dataloader = DataLoader(
+        pin_memory=True,
+        batch_size=batch_size,
+        num_workers=num_data_workers,
+        dataset=InMemoryDataset(data),
+        collate_fn=DataCollator(tokenizer),
     )
-    output_dir: Path = field(
-        metadata={"help": "An output directory to save the embeddings"},
-    )
-    model: str = field(
-        default="facebook/esm2_t6_8M_UR50D",
-        metadata={"help": "Model name or path"},
-    )
-    batch_size: int = field(default=8, metadata={"help": "Inference batch size"})
-    num_data_workers: int = field(
-        default=4, metadata={"help": "Number of data workers for batching"}
-    )
+
+    # Compute averaged hidden embeddings
+    avg_embeddings = compute_avg_embeddings(model, dataloader)
+
+    # Save or use the averaged embeddings as needed
+    # For example, you can save the embeddings to a file
+    np.save(output_dir / f"{file.name}-embeddings.npy", avg_embeddings)
+
+
+@register()
+def get_esm_model(model_id: str) -> tuple[PreTrainedModel, PreTrainedTokenizer]:
+    """Initialize the model and tokenizer, subsequent calls will be warmstarts."""
+    # Load model and tokenizer
+    tokenizer = EsmTokenizer.from_pretrained(model_id)
+    model = EsmForMaskedLM.from_pretrained(model_id, torch_dtype=torch.float16)
+    model.eval()
+
+    # Load the model onto the device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    # Compile the model for faster inference
+    model = torch.compile(model, fullgraph=True)
+
+    return model, tokenizer
+
+
+def fasta_data_reader(data_file: Path) -> list[str]:
+    return [" ".join(seq.sequence.upper()) for seq in read_fasta(data_file)]
+
+
+class Config(BaseModel):
+    input_dir: Path
+    """An input directory containing .fasta files."""
+    output_dir: Path
+    """An output directory to save the embeddings."""
+    model: str = "facebook/esm2_t6_8M_UR50D"
+    """Model name or path."""
+    num_data_workers: int = 4
+    """Number of data workers for batching."""
+    batch_size: int = 8
+    """Inference batch size."""
+    compute_settings: ComputeSettingsTypes
+    """Settings for the parsl compute backend."""
 
 
 if __name__ == "__main__":
     # Parse arguments from the command line
-    args = Arguments.from_cli()
+    parser = ArgumentParser(description="Embed protein sequences using ESM")
+    parser.add_argument(
+        "--config",
+        type=Path,
+        required=True,
+        help="Path to the .yaml configuration file",
+    )
+    args = parser.parse_args()
 
-    # Load model and tokenizer
-    tokenizer = EsmTokenizer.from_pretrained(args.model)
-    model = EsmForMaskedLM.from_pretrained(args.model, torch_dtype=torch.float16)
-    model.eval()
-    model.to(distributed_state.device)
-    model = torch.compile(model, fullgraph=True)
-
-    print(model)
+    # Load the configuration
+    config = Config.from_yaml(args.config)
 
     # Collect all sequence files
-    input_files = list(args.input_dir.glob("*"))
+    input_files = list(config.input_dir.glob("*"))
 
     # Make the output directory
-    args.output_dir.mkdir(exist_ok=True)
+    config.output_dir.mkdir(exist_ok=True)
+
+    # Set the static arguments of the worker function
+    worker_fn = functools.partial(
+        embed_file,
+        output_dir=config.output_dir,
+        model_id=config.model,
+        batch_size=config.batch_size,
+        num_data_workers=config.num_data_workers,
+        data_reader_fn=fasta_data_reader,
+        model_fn=get_esm_model,
+    )
+
+    # Set the parsl compute settings
+    parsl_config = config.compute_settings.get_config(config.output_dir / "parsl")
 
     # Distribute the input files across processes
-    with distributed_state.split_between_processes(input_files) as files:
-        files: list[Path]  # Spliting the list removes the type information
-
-        # Loop over the files assigned to this process
-        for file in tqdm(files, desc="Processing files"):
-            # Read fasta file sequences into a list
-            sequences = [seq.sequence for seq in read_fasta(file)]
-
-            # Build a torch dataset for efficient batching
-            dataloader = DataLoader(
-                num_workers=args.num_data_workers,
-                pin_memory=True,
-                batch_size=args.batch_size,
-                dataset=SequenceDataset(sequences),
-                collate_fn=SequenceCollator(tokenizer),
-            )
-
-            # Compute averaged hidden embeddings
-            avg_embeddings = compute_avg_embeddings(model, dataloader)
-
-            # Save or use the averaged embeddings as needed
-            # For example, you can save the embeddings to a file
-            np.save(args.output_dir / f"{file.name}-embeddings.npy", avg_embeddings)
+    with ParslPoolExecutor(parsl_config) as pool:
+        pool.map(worker_fn, input_files)
--- protein_search/utils.py
+++ protein_search/utils.py
@@ -1,13 +1,80 @@
+"""Utilities for protein_search."""
 from __future__ import annotations
 import re
 from argparse import ArgumentParser
 from dataclasses import dataclass, field, fields, MISSING
 from pathlib import Path
 from typing import Union, TypeVar, Type, get_type_hints
+import json
+import yaml
+from pydantic import BaseModel as _BaseModel
+
 
 PathLike = Union[str, Path]
 
 T = TypeVar("T")
+
+
+class BaseModel(_BaseModel):
+    """An interface to add JSON/YAML serialization to Pydantic models."""
+
+    def write_json(self, path: PathLike) -> None:
+        """Write the model to a JSON file.
+
+        Parameters
+        ----------
+        path : str
+            The path to the JSON file.
+        """
+        with open(path, "w") as fp:
+            json.dump(self.dict(), fp, indent=2)
+
+    @classmethod
+    def from_json(cls: type[T], path: PathLike) -> T:
+        """Load the model from a JSON file.
+
+        Parameters
+        ----------
+        path : str
+            The path to the JSON file.
+
+        Returns:
+        -------
+        T
+            A specific BaseModel instance.
+        """
+        with open(path) as fp:
+            data = json.load(fp)
+        return cls(**data)
+
+    def write_yaml(self, path: PathLike) -> None:
+        """Write the model to a YAML file.
+
+        Parameters
+        ----------
+        path : str
+            The path to the YAML file.
+        """
+        with open(path, mode="w") as fp:
+            yaml.dump(json.loads(self.json()), fp, indent=4, sort_keys=False)
+
+    @classmethod
+    def from_yaml(cls: type[T], path: PathLike) -> T:
+        """Load the model from a YAML file.
+
+        Parameters
+        ----------
+        path : PathLike
+            The path to the YAML file.
+
+        Returns:
+        -------
+        T
+            A specific BaseModel instance.
+        """
+        with open(path) as fp:
+            raw_data = yaml.safe_load(fp)
+        return cls(**raw_data)
 
 
 class ArgumentsBase:
