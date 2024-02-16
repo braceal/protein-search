a1c17a8cb7ae331e2767f78168a2af81ac4db66b
braceal <abrace05@gmail.com>
2024-02-15 20:35:33 -0600

    cli refactor

--- protein_search/cli.py
+++ protein_search/cli.py
@@ -94,28 +94,16 @@ def build_faiss_index(
         },
     )
 
-    #breakpoint()
-    #index = faiss.index_factory(320, "Flat", faiss.METRIC_INNER_PRODUCT)
-    #index = faiss.index_cpu_to_all_gpus(index, useFloat16=True)
-    #embs = [emb["embeddings"] in generate_dataset(fasta_files, embedding_files)]
-    #index.add(embs)
-    #dataset.add_faiss_index("embeddings", custom_index=index)
-
-
     # Build the FAISS index
     dataset.add_faiss_index(
         column='embeddings',
         index_name='embeddings',
         faiss_verbose=True,
-        metric_type=faiss.METRIC_L2, #faiss.METRIC_INNER_PRODUCT,
-        #string_factory="IndexHNSWFlat",
-        #dtype=np.float16,
+        metric_type=faiss.METRIC_L2,
     )
-    #breakpoint()
     # Save the dataset to disk
-    #dataset.save_faiss_index('embeddings', dataset_dir)
-    dataset.save_faiss_index('embeddings', dataset_dir.with_suffix(".index"))
-    dataset.drop_index("embeddings")
+    dataset.save_faiss_index('embeddings', dataset_dir.with_suffix('.index'))
+    dataset.drop_index('embeddings')
     dataset.save_to_disk(dataset_dir)
 
 
@@ -125,7 +113,7 @@ def search(  # noqa: PLR0913
         ...,
         '--dataset_dir',
         '-d',
-        help='The path to the dataset file.',
+        help='The path to the dataset directory.',
     ),
     query_file: Path = typer.Option(  # noqa: B008
         ...,
@@ -159,17 +147,16 @@ def search(  # noqa: PLR0913
     ),
 ) -> None:
     """Search for similar sequences."""
-    from datasets import Dataset
     import numpy as np
+    from datasets import Dataset
 
     from protein_search.distributed_inference import embed_file
     from protein_search.distributed_inference import fasta_data_reader
     from protein_search.distributed_inference import get_esm_model
 
     # Load the dataset from disk
-    #dataset = Dataset.load_from_disk(dataset_dir.as_posix())
     dataset = Dataset.load_from_disk(dataset_dir)
-    dataset.load_faiss_index('embeddings', dataset_dir.with_suffix(".index"))
+    dataset.load_faiss_index('embeddings', dataset_dir.with_suffix('.index'))
 
     # Get the embeddings for the query sequences
     query_embeddings = embed_file(
@@ -179,37 +166,22 @@ def search(  # noqa: PLR0913
         num_data_workers=num_data_workers,
         data_reader_fn=fasta_data_reader,
         model_fn=get_esm_model,
-    ).astype(np.float32)
+    )
 
+    # Convert the query embeddings to float32 for FAISS
+    query_embeddings = query_embeddings.astype(np.float32)
 
-    # Perform the search
-    #all_scores, all_samples = [], []
-    #for query_embedding in query_embeddings:
-    #    scores, samples = dataset.get_nearest_examples(
-    #        'embeddings',
-    #        query_embedding,
-    #        k=top_k,
-    #    )
-    #    all_scores.append(scores)
-    #    all_samples.append(samples)
-
-    # Read the query sequences
-    # sequences = read_fasta(query_file)
-    # raw_sequences = [sequence.sequence for sequence in sequences]
-    #breakpoint() 
-    #all_scores, all_indices = [], []
-    #for query_embedding in query_embeddings:
-    #    scores, indices = dataset.search(index_name="embeddings", query=query_embedding.reshape(-1, 1), k=top_k)
-    #    all_scores.append(scores)
-    #    all_indices.append(indices)
-
-    out = dataset.search_batch(index_name="embeddings", queries=query_embeddings, k=top_k)
+    # Search the dataset for the top k similar sequences
+    out = dataset.search_batch(
+        index_name='embeddings',
+        queries=query_embeddings,
+        k=top_k,
+    )
 
     # Print the results
     for score, ind in zip(out.total_scores, out.total_indices):
         print(score, ind)
 
-    #breakpoint()
 
 def main() -> None:
     """Entry point for CLI."""
